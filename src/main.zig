1e047bfec4ccbbd7f66691a36da2ef4868ff8dc8
QuaternionWake <simep@proton.me>
2025-02-21 22:55:29 +0100

    Implement background grid

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const rl = @import("raylib");
 const rg = @import("raygui");
 
 const Color = rl.Color;
+const Vec2 = rl.Vector2;
 const Rect = rl.Rectangle;
 
 var screen_width: i32 = 800;
@@ -25,6 +26,16 @@ pub fn main() !void {
         @floatFromInt(screen_height),
     );
 
+    var camera = rl.Camera2D{
+        .offset = Vec2.zero(),
+        .target = Vec2.zero(),
+        .rotation = 0,
+        .zoom = 20,
+    };
+
+    var holding_grid = false;
+    var holding_sidebar = false;
+
     while (!rl.windowShouldClose()) {
         if (updateScreenSize()) {
             sidebar_rect = Rect.init(
@@ -35,12 +46,64 @@ pub fn main() !void {
             );
         }
 
+        const mouse_pos = rl.getMousePosition();
+        const pointer_pos = camera.target.add(mouse_pos.scale(1 / camera.zoom));
+        const scroll = rl.getMouseWheelMove();
+
+        if (!holding_grid and (holding_sidebar or rl.checkCollisionPointRec(mouse_pos, sidebar_rect))) {
+            holding_sidebar = rl.isMouseButtonDown(rl.MouseButton.left);
+        }
+
+        if (!holding_sidebar and (holding_grid or !rl.checkCollisionPointRec(mouse_pos, sidebar_rect))) {
+            holding_grid = rl.isMouseButtonDown(rl.MouseButton.left);
+            if (holding_grid) {
+                const delta = rl.getMouseDelta().scale(-1 / camera.zoom);
+                camera.target = camera.target.add(delta);
+            }
+
+            if (scroll != 0) {
+                camera.zoom += scroll;
+                if (camera.zoom < 0.1) {
+                    camera.zoom = 0.1;
+                } else if (camera.zoom > 100) {
+                    camera.zoom = 100;
+                }
+
+                camera.target = pointer_pos.subtract(mouse_pos.scale(1 / camera.zoom));
+            }
+        }
+
         rl.beginDrawing();
-        rl.clearBackground(Color.ray_white);
+        {
+            rl.clearBackground(Color.ray_white);
 
-        _ = rg.guiPanel(sidebar_rect, "Options");
+            camera.begin();
+            {
+                drawGrid(camera);
+                rl.drawRectangle(-1, -1, 2, 2, Color.sky_blue);
+            }
+            camera.end();
 
+            _ = rg.guiPanel(sidebar_rect, "Options");
+        }
         rl.endDrawing();
+    }
+}
+
+fn drawGrid(camera: rl.Camera2D) void {
+    const start_x: i32 = @as(i32, @intFromFloat(camera.target.x)) - 1;
+    const start_y: i32 = @as(i32, @intFromFloat(camera.target.y)) - 1;
+    const end_x: i32 = @as(i32, @intFromFloat(camera.target.x + @as(f32, @floatFromInt(screen_width)) / camera.zoom)) + 1;
+    const end_y: i32 = @as(i32, @intFromFloat(camera.target.y + @as(f32, @floatFromInt(screen_height)) / camera.zoom)) + 1;
+
+    var x = start_x;
+    while (x < end_x) : (x += 1) {
+        rl.drawLine(x, start_y, x, end_y, Color.gray);
+    }
+
+    var y = start_y;
+    while (y < end_y) : (y += 1) {
+        rl.drawLine(start_x, y, end_x, y, Color.gray);
     }
 }
 
